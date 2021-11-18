da2b12801eecca2445ff1e2fea339c657d7cd81e
Salvatore Impellizzeri <Salvatore.Impellizzeri@coreview.com>
2021-11-18 21:25:13 +0100

    first commit

---
+++ src/main/java/com/vodafone/api/VodafoneApplication.java
@@ -0,0 +1,13 @@
+package com.vodafone.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VodafoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VodafoneApplication.class, args);
+	}
+
+}
