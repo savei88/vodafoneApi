9ed7e904d9e99af7f95921fd985e1d9d4116591c
Salvatore Impellizzeri <Salvatore.Impellizzeri@coreview.com>
2021-11-18 23:47:21 +0100

    date format

--- src/main/java/com/vodafone/api/configurations/WebConfig.java
+++ src/main/java/com/vodafone/api/configurations/WebConfig.java
@@ -1,14 +1,28 @@
 package com.vodafone.api.configurations;
 
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.TimeZone;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
 @Configuration
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+	
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
@@ -19,6 +33,17 @@ public class WebConfig implements WebMvcConfigurer {
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		ObjectMapper webObjectMapper = objectMapper.copy();
+		webObjectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+		sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+		webObjectMapper.setDateFormat(sdf);
+		converters.add(new MappingJackson2HttpMessageConverter(webObjectMapper));
+		converters.add(new ByteArrayHttpMessageConverter());
 	}
 
 }
