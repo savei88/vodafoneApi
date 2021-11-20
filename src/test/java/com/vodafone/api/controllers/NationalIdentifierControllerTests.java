92dc30c4234182710099d9cf4fc3427c0c39f2ac
Salvatore Impellizzeri <Salvatore.Impellizzeri@coreview.com>
2021-11-20 23:26:03 +0100

    added test

--- src/test/java/com/vodafone/api/controllers/NationalIdentifierControllerTests.java
+++ src/test/java/com/vodafone/api/controllers/NationalIdentifierControllerTests.java
@@ -2,6 +2,7 @@ package com.vodafone.api.controllers;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 import org.junit.jupiter.api.Test;
@@ -10,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import com.vodafone.api.dtos.CalculateNationalIdentifierInputDTO;
 import com.vodafone.api.dtos.NationalIdentifierValidationDTO;
 import com.vodafone.api.entities.City;
 import com.vodafone.api.repositories.CityRepository;
@@ -26,7 +28,7 @@ public class NationalIdentifierControllerTests {
 	private CityRepository cityRepository;
 
 	@Test
-	void getNationalIdentifiers() {
+	void checkNationalIdentifier() {
 		City city = new City();
 		city.setCadastralCode("H501");
 		city.setIdentifier("XXX");
@@ -38,6 +40,22 @@ public class NationalIdentifierControllerTests {
 				.checkNationalIdentifier(rightNationalIdentifier);
 
 		assertEquals(true, result.isValid());
+	}
+
+	@Test
+	void calculateNationalIdentifier() {
+		City city = new City();
+		city.setCadastralCode("H501");
+		city.setIdentifier("XXX");
+		city.setName("ROMA");
+		city.setProvince("RO");
+		Mockito.when(cityRepository.findByNameAndProvince("Roma", "RO")).thenReturn(Arrays.asList(city));
+
+		String result = nationalIdentifierController
+				.calculateNationalIdentifier(new CalculateNationalIdentifierInputDTO("PROVA", "PROVA", true,
+						LocalDate.parse("1980-01-01"), "Roma", "RO"));
+
+		assertEquals(rightNationalIdentifier, result);
 	}
 
 }
